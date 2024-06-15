f53ea97466a95dfa507c817b6ed0e82f40662249
ebmarque <ebenezermarquezine@gmail.com>
2024-06-15 20:01:16 +0100

    new tests included

--- ex02/sources/main.cpp
+++ ex02/sources/main.cpp
@@ -6,57 +6,120 @@
 /*   By: ebmarque <ebmarque@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 19:50:47 by ebmarque          #+#    #+#             */
-/*   Updated: 2024/06/15 19:53:25 by ebmarque         ###   ########.fr       */
+/*   Updated: 2024/06/15 20:00:36 by ebmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/MutantStack.hpp"
+#include <list>
 
 int main()
 {
-	// Create a MutantStack of integers
-	MutantStack<int> mStack;
+	// // Create a MutantStack of integers
+	// MutantStack<int> mStack;
 
-	std::cout << "MutantStack of integers has been created.\n";
+	// std::cout << "MutantStack of integers has been created.\n";
 
-	// Push elements onto the stack
-	for (int i = 1; i <= 10; ++i)
+	// // Push elements onto the stack
+	// for (int i = 1; i <= 10; ++i)
+	// {
+	// 	mStack.push(i * 10);
+	// 	std::cout << "Pushed " << i * 10 << " onto the stack.\n";
+	// }
+
+	// // Access the top element
+	// std::cout << "Top element of the stack: " << mStack.top() << "\n";
+
+	// // Size of the stack
+	// std::cout << "Size of the stack: " << mStack.size() << "\n";
+
+	// // Pop an element
+	// mStack.pop();
+	// std::cout << "Popped the top element. New top: " << mStack.top() << "\n";
+
+	// // Use the iterator to print all elements
+	// std::cout << "All elements in the stack from bottom to top: ";
+	// for (MutantStack<int>::iterator it = mStack.begin(); it != mStack.end(); ++it)
+	// {
+	// 	std::cout << *it << " ";
+	// }
+	// std::cout << "\n";
+
+	// // Copy the stack
+	// MutantStack<int> mStackCopy = mStack;
+	// std::cout << "Copied the stack. Top element of the copied stack: " << mStackCopy.top() << "\n";
+
+	// // Compare the top elements of the original and copied stacks
+	// if (mStack.top() == mStackCopy.top())
+	// {
+	// 	std::cout << "Top elements of the original and copied stacks are equal.\n";
+	// }
+	// else
+	// {
+	// 	std::cout << "Top elements of the original and copied stacks are not equal.\n";
+	// }
+
+	// return 0;
+	std::cout << "/ ***** MutantStack ***** /" << std::endl;
+
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+
+	while (it != ite)
 	{
-		mStack.push(i * 10);
-		std::cout << "Pushed " << i * 10 << " onto the stack.\n";
+		std::cout << *it << std::endl;
+		++it;
 	}
+	std::stack<int> s(mstack);
 
-	// Access the top element
-	std::cout << "Top element of the stack: " << mStack.top() << "\n";
+	/* -------------------------------------------------------------------- */
 
-	// Size of the stack
-	std::cout << "Size of the stack: " << mStack.size() << "\n";
+	std::cout << std::endl;
+	std::cout << "/ ***** List ***** /" << std::endl;
 
-	// Pop an element
-	mStack.pop();
-	std::cout << "Popped the top element. New top: " << mStack.top() << "\n";
+	std::list<int> lst;
 
-	// Use the iterator to print all elements
-	std::cout << "All elements in the stack from bottom to top: ";
-	for (MutantStack<int>::iterator it = mStack.begin(); it != mStack.end(); ++it)
+	lst.push_back(5);
+	lst.push_back(17);
+
+	std::cout << lst.back() << std::endl;
+
+	lst.pop_back();
+	std::cout << lst.size() << std::endl;
+
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	//[...]
+	lst.push_back(0);
+
+	std::list<int>::iterator lst_it = lst.begin();
+	std::list<int>::iterator lst_ite = lst.end();
+	++it;
+	--it;
+	while (lst_it != lst_ite)
 	{
-		std::cout << *it << " ";
+		std::cout << *lst_it << std::endl;
+		++lst_it;
 	}
-	std::cout << "\n";
-
-	// Copy the stack
-	MutantStack<int> mStackCopy = mStack;
-	std::cout << "Copied the stack. Top element of the copied stack: " << mStackCopy.top() << "\n";
-
-	// Compare the top elements of the original and copied stacks
-	if (mStack.top() == mStackCopy.top())
-	{
-		std::cout << "Top elements of the original and copied stacks are equal.\n";
-	}
-	else
-	{
-		std::cout << "Top elements of the original and copied stacks are not equal.\n";
-	}
-
-	return 0;
+	std::list<int> l(lst);
+	return (0);
 }
